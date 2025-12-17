a968434632726f287472f47f19fc1858a34eaba6
Burkhansss7 <252174@astanait.edu.kz>
2025-12-17 12:09:09 +0500

    Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Library {
+    private String libraryName;
+    private ArrayList<book> books;
+
+    public Library(String libraryName) {
+        this.libraryName = libraryName;
+        this.books = new ArrayList<>();
+    }
+
+    public String getLibraryName() { return libraryName; }
+    public void setLibraryName(String libraryName) { this.libraryName = libraryName; }
+
+    public void addBook(book book) {
+        books.add(book);
+    }
+
+    public void printBooks() {
+        System.out.println("Library: " + libraryName);
+        for (book b : books) {
+            b.printInfo();
+        }
+    }
 }
